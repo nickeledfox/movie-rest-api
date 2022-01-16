07ee5ffe4dbf44b500e7b28151e88599bc811cd4
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2022-01-16 15:16:49 -0800

    Starter point

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const routes = require("./server/routes/movie.js");
+app.use("/", routes);
+
+app.listen(port, () => console.log(`Listening on port ${PORT}`));
---
+++ server/routes/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/movie");
+
+// routes
+router.get("/");
+
+module.exports = router;
